7601a6c85ae01062af51cb8c538660b7d88d6c4d
Dell <Dell@DESKTOP-16O7D5M>
2022-08-24 13:10:09 +0530

    bfdfs

---
+++ src/test/java/gitHubPractise01/This_is_GitHub_Practise.java
@@ -0,0 +1,8 @@
+package gitHubPractise01;
+
+public class This_is_GitHub_Practise {
+
+	public static void main(String[] args) {
+		System.out.println("hii this is github prastise");
+	}
+}
---
+++ src/test/java/gitHubPractise02/GitHub_practise02.java
@@ -0,0 +1,9 @@
+package gitHubPractise02;
+
+public class GitHub_practise02 {
+
+	public static void main(String[] args) {
+		System.out.println("GitHub Practise");
+	}
+
+}
